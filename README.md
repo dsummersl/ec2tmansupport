e2f2f3700db803174fed79d64a157aab2aaa1f38
Dane Summers <dsummersl@yahoo.com>
2013-01-20 10:31:02 -0500

    partial multi-panes per window support

---
+++ README.md
@@ -0,0 +1,31 @@
+VT Ansible
+====
+
+Support scripts for using ansible on top of ec2.
+
+Scripts
+====
+
+__find.sh__ -- simple script that searches all groups in ec2 by your regex.
+
+    # find all prod groups/keys
+    ./find.sh prod
+
+ansible. Use ansible with ec2 by specifying the ec2 script to search boxes.
+
+    # search all hosts for the current version of puppet. Save to an out file as json.
+    ansible -i ./ec2.py -u dsummers all -m command -a "puppet --version" -t out
+
+    # use underscore-cli to aggregate the results.
+    (for i in out/*; do cat $i | underscore select .stdout; done) | sort | uniq -c
+
+__tmux.sh__ -- open up several SSH connections in a new tmux session.
+
+Requirements
+====
+
+npm install -g underscore-cli
+
+tmux
+
+Ansible
--- vtmux.sh
+++ vtmux.sh
@@ -4,11 +4,12 @@ set -e
 set -u
 
 help() {
-  echo "Usage: ./vtmux.sh [-l][-s name][-u user] host-pattern"
+  echo "Usage: ./vtmux.sh [-l][-s name][-u user][-p num] host-pattern"
   echo ""
   echo "Connect to several hosts in a new tmux session."
   echo ""
   echo "Options:"
+  echo " -p : Max SSH panes per window (defaults to 0 == no max)"
   echo " -l : Just list hosts that match your pattern and exit"
   echo " -s : tmux session name. Defaults to the 'ec2hosts'"
   echo " -u : ssh username"
@@ -29,8 +30,12 @@ fi
 LISTONLY=0
 TMUXSESSION='ec2hosts'
 SSHUSER='root'
+MAXPANECOUNT=0
 while getopts "ls:u:" opt; do
   case $opt in
+    p )
+      MAXPANECOUNT=$OPTARG
+      ;;
     u )
       SSHUSER="$OPTARG"
       ;;
@@ -68,19 +73,36 @@ if [[ $LISTONLY -eq 1 ]]; then
 else
   # TODO verify that the session doesn't already exist.
   tmux new-session -d -s "$TMUXSESSION"
+
+  # turn on window activity notification:
+  tmux set-window-option -t "$TMUXSESSION" -g monitor-activity on
+  tmux set-option -t "$TMUXSESSION" -g visual-activity on
+  # TODO session name.
+  tmux bind-key e command-prompt -p "message?" "run-shell \"./lib/execute_everywhere.sh '%1'\""
+
   cnt=1
+  wcnt=0
   while read line
   do
     # pull the actual host name from whatever the tag name was:
     hostname=`cat /tmp/ansible-ec2.cache | underscore select .$line | underscore process "console.log(data[0][0])"`
-    tmux new-window -t "$TMUXSESSION:$cnt"
-    tmux rename-window -t "$TMUXSESSION:$cnt" $line
-    tmux set-window-option -t "$TMUXSESSION:$cnt" allow-rename off
-    tmux send-keys -t "$TMUXSESSION:$cnt" "ssh -o StrictHostKeyChecking=no $SSHUSER@$hostname" C-m
-    let cnt=$cnt+1
+    if [ $cnt -lt $MAXPANECOUNT -o $MAXPANECOUNT -eq 0 ]; then
+      if [[ $cnt -gt 1 ]]; then
+        tmux split-window -t "$TMUXSESSION:$wcnt"
+      fi
+      let cnt=$cnt+1
+    else
+      tmux new-window -t "$TMUXSESSION:$wcnt"
+      tmux rename-window -t "$TMUXSESSION:$wcnt" $line
+      tmux set-window-option -t "$TMUXSESSION:$wcnt" allow-rename off
+      let wcnt=$wcnt+1
+      let cnt=1
+    fi
+    tmux send-keys -t "$TMUXSESSION:$wcnt" "ssh -o StrictHostKeyChecking=no $SSHUSER@$hostname" C-m
+
+    tmux select-layout -t "$TMUXSESSION:$wcnt" tiled
   done < matches.txt
   # remove session 0 - which is not connected to anything
-  tmux kill-window -t "$TMUXSESSION:0"
   # TODO provide a hotkey to run in all sessions
   # TODO provide a key to collapse all windows into X panes per window
   # TODO provide a key to move all panes out
